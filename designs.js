3a52904d6a0f0146c50ff40ab11cb5bc311d5140
eYinka <olaryeankarh@gmail.com>
2018-07-01 17:32:08 +0100

    Version 1.0 of the Udacity Pixel art maker. Might need to style further.

---
+++ designs.js
@@ -0,0 +1,49 @@
+// I declared all necessary variables with "let" as instructed 
+
+// Assign table grid
+let canvas = document.getElementById("pixelCanvas"); 
+// Assign rows
+let height = document.getElementById("inputHeight");
+// Assign width
+let width = document.getElementById("inputWidth");
+// Assign my form data
+let sizePicker = document.getElementById("sizePicker");
+// Assign color input
+let color = document.getElementById("colorPicker");
+
+
+// Select color & size input process
+
+color.addEventListener("click", function(){});
+
+sizePicker.onsubmit = function(evt){
+    evt.preventDefault();
+    clearGrid();
+    makeGrid();
+};
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+  // Your code goes here!
+
+    for (let rw=0; rw<height.value; rw++){
+        const row = canvas.insertRow(rw);
+        for (let cl=0; cl<width.value; cl++){
+            const cell = row.insertCell(cl);
+            cell.addEventListener("click", fillSquare);
+        }
+    }
+}
+
+function clearGrid(){
+    while (canvas.firstChild){
+         canvas.removeChild(canvas.firstChild);
+    }
+}
+
+function fillSquare () {
+    this.setAttribute("style", `background-color: ${color.value}`);
+}
+
